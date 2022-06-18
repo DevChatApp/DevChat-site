1de3201fcb66c1b36b3263be2e20f6c297e93f5d
Thibaut009 <gabet.thibaut@gmail.com>
2022-06-18 15:28:44 +0200

    dark mode

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,21 +9,35 @@ import Help from './Components/Pages/Help';
 import Offer from './Components/Pages/Offer';
 import Register from './Components/Pages/Register';
 
+import DarkModeToggle from "react-dark-mode-toggle";
+import {createContext, useState} from "react";
+export const ThemeContext = createContext(null);
+
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/help" element={<Help />} />
-          <Route path="/offer" element={<Offer />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </Router>
-    </div>
+    <ThemeContext.Provider value={{theme, setTheme}}>
+      <div className="App" id={theme}>
+        <Router>
+          <Navbar /> 
+          <div className="switch">
+            <DarkModeToggle onChange={toggleTheme} checked={theme === "dark"} size={60}/>
+          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/help" element={<Help />} />
+            <Route path="/offer" element={<Offer />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </Router>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
