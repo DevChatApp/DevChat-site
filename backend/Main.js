1fdc9cf22468ebf9662fa244f304baf1ccb2820a
Thibaut009 <gabet.thibaut@gmail.com>
2022-07-04 13:54:16 +0200

    add mysql

--- backend/Main.js
+++ backend/Main.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const mysql = require('mysql');
+  
 const app = express();
-const path = require('path');
-
-app.use(express.static(path.join(__dirname, 'build')))
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+const PORT = 3000;
+  
+// Create a connection to the database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: "DevChat"        
 });
-
-app.listen(3000);
+  
+// open the MySQL connection
+connection.connect(error => {
+    if (error){
+        console.log("A error has been occured "
+            + "while connecting to database.");        
+        throw error;
+    }
+      
+    //If Everything goes correct, Then start Express Server
+    app.listen(PORT, ()=>{
+        console.log("Database connection is Ready and "
+             + "Server is Listening on Port ", PORT);
+    })
+});
